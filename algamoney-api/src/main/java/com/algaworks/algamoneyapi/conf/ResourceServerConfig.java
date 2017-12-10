3c4d3ff61a8b73892e2703831126ebca3ec7d5c3
Mauricio <mauricionrgarcia@gmail.com>
2017-12-10 18:23:57 -0200

    Movendo o usuário para o banco de dados

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/conf/ResourceServerConfig.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/conf/ResourceServerConfig.java
@@ -1,18 +1,22 @@
 package com.algaworks.algamoneyapi.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
- * Classe de configuração para autentificação local dos recursos 
+ * Classe de configuração para autentificação local dos recursos
  *
  * @author <a href="mailto:mauricionrgarcia@gmail.com">Mauricio</a>
  * @version
@@ -24,26 +28,41 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	/**
+	 * @see
+	 */
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	/**
 	 * @param auth
 	 * @throws Exception
 	 */
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		// auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/launches/**").permitAll()
-				.anyRequest().authenticated().and().httpBasic()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/launches/**").permitAll().anyRequest().authenticated()
+				.and().httpBasic()
 				// não maanter sessão
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().csrf().disable();
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
+				.disable();
 	}
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.stateless(Boolean.TRUE);
+	}
+
+	/**
+	 * @return PasswordEncoder
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/security/util/AppUserDetailServiceImpl.java
@@ -0,0 +1,54 @@
+package com.algaworks.algamoneyapi.security.util;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algamoneyapi.model.User;
+import com.algaworks.algamoneyapi.repository.UserRepository;
+
+/**
+ * Inplmementation of {@link UserDetailsService}
+ *
+ * @author <a href="mailto:mauricionrgarcia@gmail.com">Mauricio</a>
+ * @version
+ * @sinse 10/12/2017 17:32:31
+ */
+@Service
+public class AppUserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private transient UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<User> optional = userRepository.findByEmail(email);
+		User user = optional.orElseThrow(() -> new UsernameNotFoundException("username.not.found="));
+		UserDetails securityUser = new org.springframework.security.core.userdetails.User(email, user.getPassword(),
+				getAuthorities(user));
+		return securityUser;
+	}
+
+	/**
+	 * prepara a lista de rules
+	 *
+	 * @param user
+	 * @return
+	 */
+	private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		user.getPermissions()
+				.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescription().toUpperCase())));
+		return authorities;
+	}
+
+}
