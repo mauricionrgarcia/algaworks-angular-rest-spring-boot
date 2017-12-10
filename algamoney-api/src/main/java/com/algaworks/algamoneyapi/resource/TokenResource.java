5c2064961bcd12b5c301f38dfd777946597d6b3c
Mauricio <mauricionrgarcia@gmail.com>
2017-12-10 21:45:20 -0200

    Solução para logout removendo o token do cookie

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/TokenResource.java
@@ -0,0 +1,36 @@
+package com.algaworks.algamoneyapi.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Adicionando funcionalidades ao controle dos tokens
+ *
+ * @author <a href="mailto:mauricionrgarcia@gmail.com">Mauricio</a>
+ * @version
+ * @sinse 10/12/2017 20:09:15
+ */
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(Boolean.TRUE);
+		cookie.setSecure(Boolean.FALSE);
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+
+	}
+
+}
