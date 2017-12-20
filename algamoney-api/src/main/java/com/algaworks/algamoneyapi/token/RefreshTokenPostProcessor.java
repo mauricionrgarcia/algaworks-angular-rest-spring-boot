7512e3b7531c51a5471489a983f18fc335f84566
Mauricio <mauricionrgarcia@gmail.com>
2017-12-19 23:50:22 -0200

    Adicionando Profiles do Spring

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/token/RefreshTokenPostProcessor.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/token/RefreshTokenPostProcessor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -16,6 +17,8 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import com.algaworks.algamoneyapi.conf.AlgamoneyAPIProperty;
+
 /**
  * Intercepta a requisição do token para remover o refresh_token do json de
  * respota e adicona o mesmo no cookie
@@ -26,6 +29,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  */
 @ControllerAdvice
 public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+	@Autowired
+	private transient AlgamoneyAPIProperty property;
 
 	@Override
 	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -70,7 +76,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 
 		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
 		refreshTokenCookie.setHttpOnly(Boolean.TRUE);
-		refreshTokenCookie.setSecure(Boolean.FALSE); // TODO - migrar para true em ambiente produtivo
+		refreshTokenCookie.setSecure(property.getSecurity().getEnagleHttps());
 		refreshTokenCookie.setPath(httpRequest.getContextPath() + "/oauth/token");
 		refreshTokenCookie.setMaxAge(2592000);
 		httpResponse.addCookie(refreshTokenCookie);
